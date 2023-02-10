f20e34caaeb034f7632fc2312469b3e0a19a7693
Kiarah Louise T. Ancajas <kiarah.ancajas@gmail.com>
2023-02-10 23:49:35 +0800

    install packages and updating database

--- fd-recruitment-task/src/Infrastructure/Persistance/Migrations/20230209125145_initialMigration.Designer.cs
+++ fd-recruitment-task/src/Infrastructure/Persistance/Migrations/20230209125145_initialMigration.Designer.cs
@@ -3,17 +3,19 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Todo_App.Infrastructure.Persistence;
 
 #nullable disable
 
-namespace Todo_App.Infrastructure.Persistence.Migrations
+namespace Todo_App.Infrastructure.Persistance.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230209125145_initialMigration")]
+    partial class initialMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ fd-recruitment-task/src/Infrastructure/Persistance/Migrations/20230209125145_initialMigration.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Todo_App.Infrastructure.Persistance.Migrations
+{
+    public partial class initialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsSoftDeleted",
+                table: "TodoLists",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsSoftDeleted",
+                table: "TodoItems",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsSoftDeleted",
+                table: "TodoLists");
+
+            migrationBuilder.DropColumn(
+                name: "IsSoftDeleted",
+                table: "TodoItems");
+        }
+    }
+}
